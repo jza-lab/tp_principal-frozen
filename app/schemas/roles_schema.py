86106a686f154dbd7fba80179c58ad9a99d1a4c2
uthomasz <yothomas2011.tye@gmail.com>
2025-10-05 02:30:12 -0300

    feat: Modelo de roles para gestion de uso del sistema.

---
+++ app/schemas/roles_schema.py
@@ -0,0 +1,12 @@
+from marshmallow import Schema, fields, validate
+
+class RoleSchema(Schema):
+    """
+    Schema para la validación de datos de roles.
+    """
+    id = fields.Int(dump_only=True)
+    codigo = fields.Str(required=True, validate=validate.Length(min=1))
+    nombre = fields.Str(required=True, validate=validate.Length(min=1))
+    nivel = fields.Int(required=True, validate=validate.Range(min=1, max=3))
+    descripcion = fields.Str(allow_none=True)
+    created_at = fields.DateTime(dump_only=True)
