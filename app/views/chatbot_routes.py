3cad3275418ad5e85dfe30e7dfc58f8cf15e31d0
uthomasz <yothomas2011.tye@gmail.com>
2025-11-10 18:30:54 -0300

    fix permiso it en chatbot

--- app/views/chatbot_routes.py
+++ app/views/chatbot_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request, render_template
 from app.controllers.chatbot_controller import ChatbotController
-from app.utils.decorators import jwt_required, roles_required
+from app.utils.decorators import jwt_required, permission_required, roles_required
 
 chatbot_bp = Blueprint('chatbot', __name__)
 chatbot_controller = ChatbotController()
@@ -19,14 +19,14 @@ def get_children_qas(parent_id):
 # --- Rutas de Administración ---
 @chatbot_bp.route('/admin/chatbot/qas', methods=['GET'], endpoint='get_all_qas_admin')
 @jwt_required()
-@roles_required(allowed_roles=['ADMIN'])
+@permission_required('admin_gestion_sistema')
 def get_all_qas():
     """Endpoint para que el admin vea todas las Q&As."""
     return chatbot_controller.get_all_qas_for_admin()
 
 @chatbot_bp.route('/admin/chatbot/qas', methods=['POST'], endpoint='create_qa_admin')
 @jwt_required()
-@roles_required(allowed_roles=['ADMIN'])
+@permission_required('admin_gestion_sistema')
 def create_qa():
     """Endpoint para que el admin cree una nueva Q&A."""
     data = request.form
@@ -34,7 +34,7 @@ def create_qa():
 
 @chatbot_bp.route('/admin/chatbot/qas/<int:qa_id>', methods=['PUT'], endpoint='update_qa_admin')
 @jwt_required()
-@roles_required(allowed_roles=['ADMIN'])
+@permission_required('admin_gestion_sistema')
 def update_qa(qa_id):
     """Endpoint para que el admin actualice una Q&A."""
     data = request.form
@@ -42,7 +42,7 @@ def update_qa(qa_id):
 
 @chatbot_bp.route('/admin/chatbot/qas/<int:qa_id>/toggle', methods=['PATCH'], endpoint='toggle_qa_admin')
 @jwt_required()
-@roles_required(allowed_roles=['ADMIN'])
+@permission_required('admin_gestion_sistema')
 def toggle_qa(qa_id):
     """Endpoint para que el admin active/desactive una Q&A."""
     data = request.form
@@ -50,15 +50,14 @@ def toggle_qa(qa_id):
 
 @chatbot_bp.route('/admin/chatbot/qas/<int:qa_id>', methods=['DELETE'], endpoint='delete_qa_admin')
 @jwt_required()
-@roles_required(allowed_roles=['ADMIN'])
+@permission_required('admin_gestion_sistema')
 def delete_qa(qa_id):
     """Endpoint para que el admin elimine (lógicamente) una Q&A."""
     return chatbot_controller.delete_qa(qa_id)
 
-# --- Ruta para la Interfaz de Administración ---
 @chatbot_bp.route('/admin/chatbot', methods=['GET'], endpoint='gestion_chatbot_page')
 @jwt_required()
-@roles_required(allowed_roles=['ADMIN'])
+@permission_required('admin_gestion_sistema')
 def gestion_chatbot_page():
     """Renderiza la página de administración del chatbot."""
     return render_template('admin/gestion_chatbot.html')
